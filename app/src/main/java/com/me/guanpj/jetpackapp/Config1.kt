ce08d7e471d0d02a28623c3bf9377853f55c0455
guanpj <guanpj.me@gmail.com>
2021-11-01 10:49:33 +0800

    aaa

---
+++ app/src/main/java/com/me/guanpj/jetpackapp/Config1.kt
@@ -0,0 +1,35 @@
+package com.me.guanpj.jetpackapp
+
+class Config1 {
+    var isFirstOpenSensorCamera = true
+
+    /**
+     * 当前处在哪个tab位置上
+     */
+    var TAB_CURRENT = 0
+
+    /**
+     * USER_ID
+     */
+    var USER_ID = ""
+
+    /**
+     * url类型
+     */
+    var PLAY_URL_TYPE = "aliCloud"
+
+    /**
+     * 用户角色
+     */
+    var USER_ROLE = "guest"
+
+    /**
+     * 设备类型
+     */
+    var DEVICE_TYPE = "mb.android"
+
+    /**
+     * 默认入口来源
+     */
+    var ENTRY = "portal"
+}
